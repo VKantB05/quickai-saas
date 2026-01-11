a8b5e448caab42fa730940a18bef4c7d2c009521
VKantB05 <vishnukantbagri@gmail.com>
2026-01-12 00:22:17 +0530

    complete home page

--- server/Server.js
+++ server/Server.js
@@ -3,9 +3,13 @@ import cors from 'cors';
 import 'dotenv/config';
 import { clerkMiddleware ,requireAuth,} from '@clerk/express'
 import router from './routes/aiRoutes.js';
+import userRouter from './routes/userRoutes.js';
+import connectCloudinary from './config/cloudinary.js';
 
 
 const app = express();
+
+await connectCloudinary();
 
 app.use(cors());
 app.use(express.json());
@@ -16,6 +20,7 @@ app.get('/', (req, res) => res.send('server is Live!'));
 app.use(requireAuth());
 
 app.use('/api/ai',router);
+app.use('/api/user',userRouter);
 
 const PORT = process.env.PORT || 3000;
 
