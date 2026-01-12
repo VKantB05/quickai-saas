ca276a323fd682771e8394a59275ac1de5f68c0b
Utkarsh Raj Singh <utkarshrajsingh30@gmail.com>
2026-01-12 14:27:15 +0530

    Dashboard is Completed

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,8 @@ import React,{useEffect, useState} from 'react'
 import { dummyCreationData } from '../assets/assets'
 import { Gem, Sparkle, Sparkles } from 'lucide-react'
 import { Protect } from '@clerk/clerk-react'
+import CreationItem from '../components/Creationitem'
+
 
 const Dashboard = () => {
   const [creations , setCreations] = useState([])
@@ -45,6 +47,9 @@ const Dashboard = () => {
 
       <div className='space-y-3'>
         <p className='mt-6 mb-4'>Recent Creations</p>
+        {
+          creations.map((item) => <CreationItem key={item.id} item={item} />)
+        }
       </div>
 
 
